17bf02bbad01c0e93313dbb8463d8a562ef044a0
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-15 04:57:59 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+#require "pry"
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +8,19 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
+def update_array_at_with(array, index, value)
+ array[index]=value
+  #return array
+end
+
+def move(board, index, character = "X")
+  #binding.pry
+  board[index] = character
+  return board
+end
